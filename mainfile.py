76131c17fd29b5007145a65cacb6128cb8ed624f
Ksenia <ilovehotpupik@internet.ru>
2025-05-26 18:45:35 +0300

    Removed punctuation, spaces, stop_words and named_entity_recognitions, also tokens were converted to lowercase and text type for a better printing

--- mainfile.py
+++ mainfile.py
@@ -9,6 +9,16 @@ doc = nlp(text)
 def get_all_tokens(doc):
     return [token.text for token in doc]
 
+def filter_token_objects(doc):
+    return [token for token in doc if token.is_alpha and not token.is_stop and not token.ent_type_]
+
+def convert_token_objects_to_lowercase(token_objects):
+    return [token.lower_ for token in token_objects]
+
+
 all_tokens = get_all_tokens(doc)
+token_objects = filter_token_objects(doc)
+filtered_tokens = convert_token_objects_to_lowercase(token_objects)
 
 print("Все токены:", get_all_tokens(doc))
+print("Токены без стоп-слов (нижний регистр):", filtered_tokens)
